91e7a1794fe3fab2ed2b754036cf5e66d2333517
Eniola Ademola <it.eniolaademola@gmail.com>
2022-08-05 17:25:20 +0100

    add more notes to the project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import noteFile from "../note";
 
 const App = () => {
-    return(
-        <>
-        <Header/>
-        <Footer/>
-        <Note/>
-        </>
-    )
-}
+	return (
+		<>
+			<Header />
+			{noteFile.map((noteItem) => (
+				<Note
+					key={noteItem.key}
+					title={noteItem.title}
+					content={noteItem.content}
+				/>
+			))}
+			<Footer />
+		</>
+	);
+};
 
-
-export default App
+export default App;

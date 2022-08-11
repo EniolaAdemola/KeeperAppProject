f88d51f8e8d453e492b52f11e5749ea7a6fa94cb
Eniola Ademola <it.eniolaademola@gmail.com>
2022-08-11 01:07:41 +0100

    update Note component to render and delete item

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import CreateArea from "./CreateArea";
 
 const Note = (props) => {
+	const handleClick = () => {
+		props.deleateItem(props.id);
+	};
+
 	return (
-		<div className="note">
-			<h1>{props.title}</h1>
-			<p>{props.content}</p>
-		</div>
+		<>
+			<div className="note">
+				<h1>{props.title}</h1>
+				<p>{props.content}</p>
+				<button onClick={handleClick}>DELETE</button>
+			</div>
+		</>
 	);
 };
 
